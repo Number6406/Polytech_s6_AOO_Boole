d9c5904ffd3444bdae2fc9d5ecd55017bf6bf3fe
Maxime <maxime.chevalier@outlook.com>
2016-04-06 17:43:08 +0200

    Charger_Sauvergarder classe

---
+++ src/Charger_Sauvegarder.java
@@ -0,0 +1,25 @@
+package src;
+
+
+public class Charger_Sauvegarder {
+	public static Circuit lireUnFichier(String nomFichier)
+	{
+		return null;
+	}
+	
+	public static void ecrireUnCircuit(Circuit c, String nomFichier)
+	{
+		
+	}
+	
+	public static Composite lireUnComposite(String nomFichier)
+	{
+		return null;
+	}
+	
+	public static void ecrireUnComposite(Composite c, String nomFichier)
+	{
+		
+	}
+
+}
