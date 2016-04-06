9c7ca1c2fa01c60d5b49c7ac4ab03483fffa00c0
Gilles B <bonhoure.gilles@gmail.com>
2016-04-06 17:40:47 +0200

    Edit du composant

--- src/$Composant.java
+++ src/$Composant.java
@@ -1,4 +1,10 @@
+import java.util.List;
+import java.util.Map;
 
 public abstract class $Composant {
 
+	private Map<PortSortie, List<PortEntree>> listeSorties;
+	private List<PortEntree> listeEntrees;
+	private String nomType;
+	
 }
