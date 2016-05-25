f51ef30012b63c4c99e6b802f5e94edba051d5de
Gilles B <bonhoure.gilles@gmail.com>
2016-05-25 14:09:43 +0200

    Juste une suppression de lignes en trop

--- src/Composite/Add4b.java
+++ src/Composite/Add4b.java
@@ -25,19 +25,16 @@ public class Add4b extends Composite {
 		this.connecter(2, 1, 4, 2); //Idem pour Gen2
 		this.connecter(8, 1, 4, 3); //1è retenue à 0 (Gnd)
 		this.connecter(4, 1, 3, 1); //PortSortie1 connecté à Aff
-		
 		//Addition sur 2è bit
 		this.connecter(1, 2, 5, 1);
 		this.connecter(2, 2, 5, 2);
 		this.connecter(4, 2, 5, 3); //La retenue est celle en sortie de l'Add précédent
 		this.connecter(5, 2, 3, 2);
-		
 		//Addition sur 3è bit
 		this.connecter(1, 3, 6, 1);
 		this.connecter(2, 3, 6, 2);
 		this.connecter(5, 2, 6, 3);
 		this.connecter(6, 1, 3, 3);
-
 		//Addition sur 4è bit
 		this.connecter(1, 4, 7, 1);
 		this.connecter(2, 4, 7, 2);
